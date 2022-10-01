b568cbdb1fc921dead42d600c6cddc70519097c9
Bharadwajkadavakooti <114815180+Bharadwajkadavakooti@users.noreply.github.com>
2022-10-01 19:58:33 +0530

    Add files via upload
    Day1 assignment

---
+++ day1/Char.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Char {
+
+	
+	// Primitive datatypes:
+	   
+		char character = 'a';
+		boolean work = true;
+		byte smallRange = 127;
+		char singelChar = '&';
+		short shortRange = 12888;
+		int till9Digits = 977777777;
+		long putAtLastL = 9777776666L;
+		float decimalPutAtLastF = 97.66F;
+		double longdecimal = 978.765432;
+		
+	//non Primitive datatypes:
+		
+		String firstName = "Bharadwaj";
+		
+		
+
+}
---
+++ day1/Factorial.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// hard coded value for factorial
+		int a = 1;
+		for (int i = 1; i <= 5; i++) {
+		a = a*i;	
+		}
+		System.out.println("Factorial of 5 is "+a);
+		
+	}
+
+}
